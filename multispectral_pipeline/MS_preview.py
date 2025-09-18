d97b90dd6934035685a5ab0b2ea353fb2ba8318f
jirka-m97 <j.machulda@seznam.cz>
2025-09-18 17:00:29 +0200

    Update MS_preview.py

--- multispectral_pipeline/MS_preview.py
+++ multispectral_pipeline/MS_preview.py
@@ -1,7 +1,7 @@
 # =========================================================================
 # RGB Camera Preview Application (Python script, en-GB)
 # Author: Jiří Mach
-# Institution: UCT Prague, Laboratory of Bioengineering
+# Institution: UCT Prague, Faculty of Food and Biochemical Technology, Laboratory of Bioengineering
 # Licence: Apache 2.0
 # Date: 2025-09-18
 # Description:
